aaf9a56b929133eddb8f8be1216c6810648870e7
秦云安“ <yunan.qin@heywh.com>
2018-09-12 20:31:28 +0800

    表单控件绑定

---
+++ form.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+	var app =  new Vue({
+		el:'#app',
+		data:{
+			message:'',
+			multimessage:'',
+			check:true,
+			checkArr:[],
+			picked:'',
+			selected:[],
+			//for 渲染列表
+			selected_v:'',
+			options: [
+		      { text: 'One', value: 'A' },
+		      { text: 'Two', value: 'B' },
+		      { text: 'Three', value: 'C' }
+		    ],
+		    toggle:'',
+		    a:'aaaaaaa',
+		    b:'bbbbbbb',
+		}
+	});
+}
