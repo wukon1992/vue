a6d5200bd82b34f2ceb5146187154b5aa1eb8291
秦云安“ <yunan.qin@heywh.com>
2018-09-12 10:37:44 +0800

    列表渲染案例组件

--- for.js
+++ for.js
@@ -6,22 +6,39 @@ window.onload=function(){
 		template:'<li>{{item}}------{{index}}</li>'
 	});
 
+	Vue.component('myitem',{
+		template: '<li> {{ title }} <button v-on:click="$emit(\'remove\')">X</button></li> ',
+  		props: ['title']
+	});
+
 	var app = new Vue({
 		el:'#app',		
 		data:{
 			message:'你好，多多指教',
 			//for 基础用法 属性定义
+			newTodoText:'',
 			items:[
 				{msg:'linux'},
 				{msg:'ios'},
 				{msg:'windows'},
 				{msg:'android'}
 			],
+			todos: [
+		      	'Do the dishes',
+		      	'Take out the trash',
+		      	'Mow the lawn'
+		    ],
 			//for 对象迭代用法
 			itemsObj: {
 				name:'秦小安',
 				oldName:'秦小龙',
 			},
+		},
+		methods:{
+			addNewTodo: function () {
+		      this.todos.push(this.newTodoText)
+		      this.newTodoText = ''
+		    }
 		}
 	});
 }
