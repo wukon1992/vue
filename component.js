66fdcdbaa40dfbbe937024213083aacb3aa5a942
秦云安“ <yunan.qin@heywh.com>
2018-09-18 18:51:59 +0800

    props 验证

--- component.js
+++ component.js
@@ -39,6 +39,42 @@ window.onload=function(){
 		}
 	})
 
+	//props 验证
+	Vue.component('check',{
+		props:{
+
+			    // 基础类型检测 （`null` 意思是任何类型都可以）
+			    propA: Number,
+			    // 多种类型
+			    propB: [String, Number],
+			    // 必传且是字符串
+			    // propC: {
+			    //   type: String,
+			    //   required: true
+			    // },
+			    // 数字，有默认值
+			    propD: {
+			      type: Number,
+			      default: 100
+			    },
+			    // 数组／对象的默认值应当由一个工厂函数返回
+			    propE: {
+			      type: Object,
+			      default: function () {
+			        return { message: 'hello' }
+			      }
+			    },
+			    // 自定义验证函数
+			    propF: {
+			      validator: function (value) {
+			        return value > 10
+			      }
+			    }	
+		},
+		// template:"<button> {{ propA }} </button>"
+		template:'<input type="text" v-bind:value="propA">'
+	});
+
 
 	var Child = {
 		template:"<h3>我是标题，标题阿三</h3>",
@@ -50,7 +86,8 @@ window.onload=function(){
 
 		data:{
 			message:'',
-			a:'BBBBB'
+			a:'BBBBB',
+			b:333
 		},
 		methods:{
 
